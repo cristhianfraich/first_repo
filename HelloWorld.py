cb0fd2944e3dbac69b9c54c6b96140bd221a6ccf
cristhian <cristhian.fraich@gmail.com>
2017-02-03 09:04:06 -0400

    Edit the message to sound more friendly

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,13 @@
-kkmkm
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 19:04:07 2017
+"""
+import numpy
+from matplotlib import pyplot
 
+a = numpy.array([1, 2, 3])
+b = a.copy()
+
+b[1] = 7
+print (a)
+print(b)
